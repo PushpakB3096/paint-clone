eed800c8ab32e9cbef8c8d804475728e62f3b28e
PushpakB3096 <rtpushpak@gmail.com>
2021-04-02 14:12:52 +0530

    Add events for handling user clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 /* getting all elements */
 const { body } = document;
 const bucketColorBtn = document.getElementById("bucket-color");
+const brushColorBtn = document.getElementById("brush-color");
+const brushIcon = document.getElementById("brush");
+const eraser = document.getElementById("eraser");
+const activeToolEl = document.getElementById("active-tool");
 
 /* global variables must be added here */
 let currentSize = 10;
 let bucketColor = "#FFFFFF";
 let currentColor = "#A51DAB";
+/**
+ * Using an eraser means that the user is 'drawing' with the canvas color.
+ * isEraser variable is going to hold whether the user is drawing on the canvas
+ * with the brush color or with the canvas color.
+ */
+let isEraser = false;
 
 // creating the main canvas element in global scope
 const canvasEl = document.createElement("canvas");
@@ -29,6 +39,18 @@ function createCanvas() {
   body.appendChild(canvasEl);
 }
 
+// function that will switch the user back to using the brush
+function switchToBrush() {
+  // user is not using the eraser
+  isEraser = false;
+  // active tool selected will show the appropriate tool selected
+  activeToolEl.textContent = "Brush";
+  brushIcon.style.color = "black";
+  eraser.style.color = "white";
+  currentColor = `#${brushColorBtn.value}`;
+  currentSize = 10;
+}
+
 /* event listeners will go here */
 
 // Setting Background Color
@@ -38,6 +60,30 @@ bucketColorBtn.addEventListener("change", () => {
   // fill the canvas with the new color selected
   createCanvas();
 });
+
+// Setting Brush Color
+brushColorBtn.addEventListener("change", () => {
+  // user has not clicked on the eraser
+  isEraser = false;
+  // setting the current color to the user selection
+  currentColor = `#${brushColorBtn.value}`;
+});
+
+// handling event when user clicks on eraser icon
+eraser.addEventListener("click", () => {
+  // user has now clicked on the eraser icon
+  isEraser = true;
+  brushIcon.style.color = "white";
+  eraser.style.color = "black";
+  // active tool selected will show the appropriate tool selected
+  activeToolEl.textContent = "Eraser";
+  // user will now 'draw' with the canvas color, essentially erasing the content
+  currentColor = bucketColor;
+  currentSize = 50;
+});
+
+// clicking on the brush icon should switch user back to using the brush
+brushIcon.addEventListener("click", switchToBrush);
 
 /* functions to call on load */
 createCanvas();
